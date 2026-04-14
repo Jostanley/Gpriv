84ce068ee32aa1ab9c9963259e6f2177af67ae2e
Jostanley <ejikejostanley@gmail.com>
2026-04-14 15:22:08 +0000

    me

--- index.js
+++ index.js
@@ -192,4 +192,4 @@ app.get("/replies", async (req, res) => {
   res.json(data);
 });
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log("Server running"));
+app.listen(PORT,"0.0.0.0" () => console.log("Server running"));
